d9156dfbfdb23d309acd6344704ead42d44f270a
Kostas Milonas <milonas.ko@gmail.com>
2014-09-29 10:40:04 +0300

    modifies query on sample profile

--- profiles/myapp.py
+++ profiles/myapp.py
@@ -7,7 +7,7 @@ db = dict(
     PASSWORD = "toor",
 
     # queries that their data will be geocoded. each will be used if previous has failed geocoding
-    QUERY = "SELECT address, address_number, area, postal_code, lat, lng FROM places WHERE lat = 0 AND lng = 0",
+    QUERY = "SELECT address, area, city, prefecture, postal_code FROM places WHERE lat = 0 AND lng = 0",
 
     # the result query template for inserts and updates
     TEMPLATE_INSERT = u"INSERT INTO places (address, area, city, prefecture, postal_code, lat, lng) VALUES ('%s', '%s', '%s', '%s', '%s', %s, %s);",
